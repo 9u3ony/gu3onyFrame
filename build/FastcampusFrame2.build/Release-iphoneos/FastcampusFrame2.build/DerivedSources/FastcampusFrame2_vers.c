5b585191df7d7bbc402a369294e0febb5f9d05f9
구본희 <gubony9@gmail.com>
2017-07-05 20:57:57 +0900

    Add .travis.yml

--- build/FastcampusFrame2.build/Release-iphoneos/FastcampusFrame2.build/DerivedSources/FastcampusFrame2_vers.c
+++
@@ -1,5 +0,0 @@
- extern const unsigned char FastcampusFrame2VersionString[];
- extern const double FastcampusFrame2VersionNumber;
-
- const unsigned char FastcampusFrame2VersionString[] __attribute__ ((used)) = "@(#)PROGRAM:FastcampusFrame2  PROJECT:FastcampusFrame2-1" "\n";
- const double FastcampusFrame2VersionNumber __attribute__ ((used)) = (double)1.;
--- build/FastcampusFrame2.build/Release-iphoneos/FastcampusFrame2.build/Objects-normal/arm64/FastcampusFrame2_vers.d
+++
@@ -1,2 +0,0 @@
-dependencies: \
-  /Users/for013/Desktop/FastCamp/FastcampusFrame2/build/FastcampusFrame2.build/Release-iphoneos/FastcampusFrame2.build/DerivedSources/FastcampusFrame2_vers.c
